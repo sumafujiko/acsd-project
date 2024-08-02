308383d43c2070ec264e53478b05a69a63926d9e
jdancey-allview <jdancey@allview.ie>
2024-08-02 19:38:12 +0100

    updating some styles and fixes filters and sorters

--- src/components/FlightDetails/FlightDetailsSorter.js
+++ src/components/FlightDetails/FlightDetailsSorter.js
@@ -4,21 +4,29 @@ import "../../sass/flightDetails.scss";
 
 const FlightDetailsSorter = ({ originalData, setFlights }) => {
   const userSelection = useState({});
-
   //doesnt neet to be state as its derived from props
+  //use a set to get only one of each
   const airportSet = new Set(
-    ...originalData
-      .map((flight) => flight?.outBoundFlight?.arrivalAirport)
+    //pass the iterable to the set
+    originalData
+      //map to only the airports - arrival airport as all leave from dub
+      .map(
+        (flight) =>
+          flight?.outboundFlight[flight.outboundFlight.length - 1]
+            ?.arrivalAirport
+      )
+      //as sum will return null, filter these out
       .filter((airport) => airport !== null && airport !== undefined)
   );
 
   const filterByAirport = (userSelection, flightsToFilter) => {
-    const airport = userSelection.airportSelector;
+    const airport = userSelection.airportSelect;
+    //filter any airport that does not match
     if (!airport) return flightsToFilter;
     const filteredFlights = flightsToFilter.filter(
       (trip) =>
         trip.outboundFlight.slice(-1)[0].arrivalAirport === airport &&
-        trip.returnFlight.slice(-1)[0].departureAirport === airport
+        (trip.returnFlight && trip.returnFlight[0].departureAirport) === airport
     );
     return filteredFlights;
   };
@@ -26,15 +34,18 @@ const FlightDetailsSorter = ({ originalData, setFlights }) => {
   const filterByMaxLayover = (userSelection, flightsToFilter) => {
     const maxLayover = userSelection.maxLayover;
     if (!maxLayover) return flightsToFilter;
-    console.log(flightsToFilter);
+
+    //filter out any flights have more segments / layovers than mentioned
     const filteredFlights = flightsToFilter.filter((trip) => {
       const outboundLayovers = trip.outboundFlight.length;
+      //return flight could be null so leave at 0
       const returnLayovers = trip.returnFlight?.length
         ? trip.returnFlight.length
         : 0;
-      return outboundLayovers <= maxLayover && returnLayovers <= maxLayover;
+      return (
+        outboundLayovers <= maxLayover + 1 && returnLayovers <= maxLayover + 1
+      );
     });
-    console.log(filteredFlights, "filtered after max layover");
     return filteredFlights;
   };
 
@@ -42,21 +53,29 @@ const FlightDetailsSorter = ({ originalData, setFlights }) => {
     const value = userSelection.priceSort;
     if (!value) return flightsToSort;
     if (value === "asc") {
+      //simple sorting function based on price
       return flightsToSort.sort((a, b) => a.price - b.price);
     } else {
+      //reverse sorting function for desc
       return flightsToSort.sort((a, b) => b.price - a.price);
     }
   };
 
+  //we take the original data and pass this to this as filtered data will cause information loss
+  //we need to perform all three actions on the data set every time any one of them is changed otherwise we are
+  //effectively resetting the other two filters
   const handleUserSelection = (e) => {
     const { name, value } = e.target;
     if (!name || !value) return;
+
+    //pass the rest of one into the other
     const newSelection = { ...userSelection, [name]: value };
     const filteredByAirport = filterByAirport(newSelection, originalData);
     const filtedByLayover = filterByMaxLayover(newSelection, filteredByAirport);
     const sortedByPrice = sortByPrice(newSelection, filtedByLayover);
-    console.log(sortedByPrice, "sortedby price");
-    setFlights(sortedByPrice);
+
+    //create a new reference by spreading to trigger an update
+    setFlights([...sortedByPrice]);
   };
 
   return (
@@ -65,7 +84,7 @@ const FlightDetailsSorter = ({ originalData, setFlights }) => {
         <div className="flight-sorter__input-container">
           <select
             name="airportSelect"
-            id="airportSelector"
+            id="airportSelect"
             onChange={handleUserSelection}
           >
             <option undefined>None Selected</option>
@@ -73,7 +92,7 @@ const FlightDetailsSorter = ({ originalData, setFlights }) => {
               <option value={airport}>{airport}</option>
             ))}
           </select>
-          <label htmlFor="airportSelector">Airport</label>
+          <label htmlFor="airportSelect">Airport</label>
         </div>
         <div className="flight-sorter__input-container">
           <select
